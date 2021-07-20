fc03e0f2d20269e8d15cc67b24fa01e9d06b3440
matt765 <mateusz.wyrebek@gmail.com>
2021-07-21 00:28:43 +0200

    react router fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,18 +2,36 @@ import { Route, Switch } from 'react-router-dom';
 import { Login, Signup, Chat } from 'components';
 import { AuthProvider } from 'context/AuthContext.js';
 import { BrowserRouter as Router } from 'react-router-dom';
-import React, {  useEffect , useState} from "react";
+import React, { useEffect, useState } from "react";
+import { fb } from "service";
 
 export const App = () => {
-  
+
+    const [mainComponent, setMainComponent] = useState("login")
+
+    useEffect(() => {
+        // This hook needs to be here because  GH Pages doesn't allow subdirectories, making app unable to refresh on subdomain
+        
+        fb.auth.onAuthStateChanged((user) => {
+            if (user && user.displayName == null) {
+                setMainComponent("chat")
+            } else if (user && user.displayName !== null) {
+                setMainComponent("chat")
+            } else {
+                setMainComponent("login")
+            }
+        });
+    }, []);
 
     return (
         <div className="app">
             <Router>
                 <AuthProvider>
                     <Switch>
-                        <Route exact path="/react-chat-app/chat" component={Chat} />
-                        <Route path="/react-chat-app" component={Login} />
+                        {(mainComponent == "login") ?
+                            <Route path="/react-chat-app/" component={Login} /> :
+                            <Route exact path="/react-chat-app/" component={Chat} />}
+
                         <Route path="/signup" component={Signup} />
                     </Switch>
                 </AuthProvider>
