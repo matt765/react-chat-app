d1f14f9d5f68aac2049aa2510b95b2cab2c270b4
matt765 <mateusz.wyrebek@gmail.com>
2021-07-26 16:08:05 +0200

    filter

--- src/components/Chat/ChatFeed/Bubble/Body.js
+++ src/components/Chat/ChatFeed/Bubble/Body.js
@@ -39,6 +39,7 @@ const Body = props => {
     text = text.replaceAll(/faggot/ig, "****")
     text = text.replaceAll(/n i g g e r/ig, "****")
     text = text.replaceAll(/n i g g a/ig, "****")
+    text = text.replaceAll(/N​I​G​G​E​R/ig, "****")
 
     return (
         <div className='ce_message' dangerouslySetInnerHTML={{ __html: text }} />
